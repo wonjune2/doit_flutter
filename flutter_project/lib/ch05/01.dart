7818492d46a76b28f5251c8321979347623bde86
Joon <zzzdnjswns@naver.com>
2023-04-21 22:36:06 +0900

    fun

--- flutter_project/lib/ch05/01.dart
+++ flutter_project/lib/ch05/01.dart
@@ -5,24 +5,47 @@
 //   some3();
 // }
 
-class MyClass {
-  void some4() {}
-  // void some4(int a) {
+// class MyClass {
+//   void some4() {}
+//   // void some4(int a) {
 
-  // }
+//   // }
+// }
+
+// void some1(int? a) {}
+
+// main() {
+//   // some2();
+//   some2(10);
+//   some2('hello');
+// }
+
+// void some2(var a) {
+//   a = 20;
+//   a = 'world';
+//   a = true;
+//   a = null;
+// }
+
+// void some3(a) {
+//   a = 20;
+//   a = 'world';
+//   a = true;
+//   a = null;
+// }
+
+dynamic some1() {
+  return 10;
 }
 
-void some1(int? a) {}
-
-main() {
-  // some2();
-  some2(10);
-  some2('hello');
+some2() {
+  return 10;
 }
 
-void some2(var a) {
-  a = 20;
-  a = 'world';
-  a = true;
-  a = null;
+some3() {}
+
+void printUser1() {
+  print('hello world');
 }
+
+void printUser2() => print('hello world');
