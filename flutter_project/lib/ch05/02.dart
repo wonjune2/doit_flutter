86dd669b98309cfb82bfbefcdd07627ed3fc1b10
Joon <zzzdnjswns@naver.com>
2023-04-22 22:09:31 +0900

    optional parameter

---
+++ flutter_project/lib/ch05/02.dart
@@ -0,0 +1,14 @@
+// void some(int a, String b, bool c) {}
+
+void main(List<String> args) {
+  // some();
+  // some('hello', true, 10);
+  // some(10, 'hello', true);
+  // some();
+}
+
+// void some({String? data1}) {
+//   print(data1);
+// }
+
+void some(int data1, {String? data2, bool data3}) {}
