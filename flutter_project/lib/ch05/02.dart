d5dcce1656eabde12468a5def8534e2122a48087
Joon <zzzdnjswns@naver.com>
2023-04-23 20:55:20 +0900

    reqired

--- flutter_project/lib/ch05/02.dart
+++ flutter_project/lib/ch05/02.dart
@@ -1,14 +1,20 @@
 // void some(int a, String b, bool c) {}
 
-void main(List<String> args) {
-  // some();
-  // some('hello', true, 10);
-  // some(10, 'hello', true);
-  // some();
-}
-
 // void some({String? data1}) {
 //   print(data1);
 // }
 
-void some(int data1, {String? data2, bool data3}) {}
+void some(int data1, {String? data2, bool? data3}) {}
+
+String myFun({String data = 'hellp'}) {
+  return data;
+}
+
+someFun({required int arg1}) {
+  print('someFun()..arg1 : $arg1');
+}
+
+void main(List<String> args) {
+  // print('myFun() result : ${myFun()}');
+  someFun(arg1: 10);
+}
