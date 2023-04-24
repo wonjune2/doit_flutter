432e932138a7373980128fe942e6f7c6476c0118
Joon <zzzdnjswns@naver.com>
2023-04-24 22:24:27 +0900

    lamda

---
+++ flutter_project/lib/ch05/04.dart
@@ -0,0 +1,33 @@
+// // void some() {}
+// Function data2 = some;
+
+// int plus(int no) {
+//   return no + 10;
+// }
+
+// int multiply(int no) {
+//   return no * 10;
+// }
+
+// Function testFun(Function argFun) {
+//   print('argFun: ${argFun(20)}');
+//   return multiply;
+// }
+
+// some(int Function(int a) f) {
+//   f(30);
+// }
+
+// void main(List<String> args) {
+//   some((int a) {
+//     return a + 20;
+//   });
+// }
+
+fun1(arg) {
+  return 10;
+}
+
+Function fun2 = (arg) {
+  return 10;
+};
