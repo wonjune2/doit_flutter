16968af5130f09dfd404bcde538d96fef447d225
Joon <zzzdnjswns@naver.com>
2023-04-28 20:07:12 +0900

    ..

--- flutter_project/lib/ch05/06.dart
+++ flutter_project/lib/ch05/06.dart
@@ -1,17 +1,33 @@
+// class User {
+//   void some() {
+//     print('User...some()');
+//   }
+// }
+
+// void main(List<String> args) {
+//   Object obj = User();
+
+//   if (obj is User) {
+//     obj.some();
+//   }
+
+//   Object obj1 = User();
+
+//   (obj1 as User).some();
+// }
+
 class User {
-  void some() {
-    print('User...some()');
+  String? name;
+  int? age;
+
+  some() {
+    print('name: $name, age: $age');
   }
 }
 
 void main(List<String> args) {
-  Object obj = User();
-
-  if (obj is User) {
-    obj.some();
-  }
-
-  Object obj1 = User();
-
-  (obj1 as User).some();
+  var user = User()
+    ..name = 'kkang'
+    ..age = 30
+    ..some();
 }
