8c732632f3483209f84058774b6e8681040ac9a2
Joon <zzzdnjswns@naver.com>
2023-04-29 21:24:30 +0900

    try/catch

--- flutter_project/lib/ch05/07.dart
+++ flutter_project/lib/ch05/07.dart
@@ -1,16 +1,45 @@
-void main(List<String> args) {
-  var list = [10, 20, 30];
-  for (var x in list) {
-    print(x);
-  }
+// void main(List<String> args) {
+//   var list = [10, 20, 30];
+//   for (var x in list) {
+//     print(x);
+//   }
+// }
+
+// some(arg) {
+//   switch (arg) {
+//     case 'A':
+//       print('A');
+//       break;
+//     case 'B':
+//       print('B');
+//   }
+// }
+
+some() {
+  throw const FormatException('my exception');
 }
 
-some(arg) {
-  switch (arg) {
-    case 'A':
-      print('A');
-      break;
-    case 'B':
-      print('B');
+void main(List<String> args) {
+  try {
+    print('step1...');
+    some();
+  } on FormatException {
+    print('step3');
+  } on Exception {
+    print('step4');
+  } finally {
+    print('step5');
+  }
+  print('step6');
+
+  try {
+    print('step1...');
+    some();
+  } on FormatException catch (e) {
+    print(e);
+  } on Exception catch (e) {
+    print('step4 $e');
+  } finally {
+    print('step5');
   }
 }
