91af4d17a8f741bea5c19755b2f06874dddbb301
Joon <zzzdnjswns@naver.com>
2023-05-03 20:41:57 +0900

    cache

--- flutter_project/lib/ch06/03.dart
+++ flutter_project/lib/ch06/03.dart
@@ -1,12 +1,12 @@
-class MyClass {
-  MyClass(int data1, int data2) {
-    print('MyClass() call...');
-  }
-  MyClass.first(int arg) : this(arg, 0);
-}
+// class MyClass {
+//   MyClass(int data1, int data2) {
+//     print('MyClass() call...');
+//   }
+//   MyClass.first(int arg) : this(arg, 0);
+// }
 
-void main(List<String> args) {
-  var obj1 = MyClass();
-  var obj2 = MyClass.first();
-  var obj3 = MyClass.second();
-}
+// void main(List<String> args) {
+//   var obj1 = MyClass();
+//   var obj2 = MyClass.first();
+//   // var obj3 = MyClass.second();
+// }
