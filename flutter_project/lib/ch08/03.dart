1c07e9a6f2dacbf29d2ae80222c116494376ef01
Joon <zzzdnjswns@naver.com>
2023-05-12 23:40:58 +0900

    statefulWIdet

--- flutter_project/lib/ch08/03.dart
+++ flutter_project/lib/ch08/03.dart
@@ -27,36 +27,72 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Stateless Test'),
         ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: changeCheck,
-                icon: (enabled
-                    ? const Icon(
-                        Icons.check,
-                        size: 20,
-                      )
-                    : const Icon(
-                        Icons.check_box_outline_blank,
-                        size: 20,
-                      )),
-                color: Colors.red,
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 16),
-                child: Text(
-                  stateText,
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
+        body: const MyWidget(),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  bool enabled = false;
+  String stateText = 'disable';
+
+  void changeCheck() {
+    setState(() {
+      if (enabled) {
+        stateText = 'disable';
+        enabled = false;
+      } else {
+        stateText = 'enable';
+        enabled = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: changeCheck,
+                  icon: (enabled
+                      ? const Icon(
+                          Icons.check,
+                          size: 20,
+                        )
+                      : const Icon(
+                          Icons.check_box_outline_blank,
+                          size: 20,
+                        )),
+                  color: Colors.red,
                 ),
-              )
-            ],
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Text(
+                    stateText,
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
