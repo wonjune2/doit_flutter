25289953cc92fcf4994f67dda336c6556dd88825
Joon <zzzdnjswns@naver.com>
2023-05-14 00:51:01 +0900

    state key

--- flutter_project/lib/ch08/06.dart
+++ flutter_project/lib/ch08/06.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class MyColorItemWidget extends StatelessWidget {
-  Color color;
-  MyColorItemWidget({
-    super.key,
-    required this.color,
-  });
+class MyREDItemWidgetState extends StatefulWidget {
+  const MyREDItemWidgetState({super.key});
 
+  @override
+  State<MyREDItemWidgetState> createState() => _MyREDItemWidgetStateState(
+        Colors.red,
+      );
+}
+
+class _MyREDItemWidgetStateState extends State<MyREDItemWidgetState> {
+  Color color;
+  _MyREDItemWidgetStateState(this.color);
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -14,6 +19,67 @@ class MyColorItemWidget extends StatelessWidget {
         color: color,
         width: 150,
         height: 150,
+      ),
+    );
+  }
+}
+
+class MyBLUEItemWidgetState extends StatefulWidget {
+  const MyBLUEItemWidgetState({super.key});
+
+  @override
+  State<MyBLUEItemWidgetState> createState() =>
+      _MyBLUEItemWidgetStateState(Colors.blue);
+}
+
+class _MyBLUEItemWidgetStateState extends State<MyBLUEItemWidgetState> {
+  Color color;
+  _MyBLUEItemWidgetStateState(this.color);
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: color,
+        width: 150,
+        height: 150,
+      ),
+    );
+  }
+}
+
+class MyListWidgetState extends StatefulWidget {
+  const MyListWidgetState({super.key});
+
+  @override
+  State<MyListWidgetState> createState() => _MyListWidgetStateState();
+}
+
+class _MyListWidgetStateState extends State<MyListWidgetState> {
+  @override
+  List<Widget> widgetList = [
+    const MyREDItemWidgetState(),
+    const MyBLUEItemWidgetState(),
+  ];
+
+  onChange() {
+    setState(() {
+      widgetList.insert(1, widgetList.removeAt(0));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Key Test'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: widgetList,
+          ),
+          ElevatedButton(onPressed: onChange, child: const Text('toggle'))
+        ],
       ),
     );
   }
