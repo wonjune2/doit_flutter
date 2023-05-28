025374c1096a24126e564e2f7d99f2c0e9209f2a
Joon <zzzdnjswns@naver.com>
2023-05-28 23:27:04 +0900

    scaffold

---
+++ flutter_project/lib/ch13/04_scaffold.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  @override
+  final List<Widget> _widgetOptions = [
+    const Text(
+      'First Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      'Second Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      'Third Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      'Fourth Screen',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(48.0),
+                child: Theme(
+                  data: Theme.of(context).copyWith(
+                      colorScheme: ColorScheme.fromSwatch()
+                          .copyWith(secondary: Colors.white)),
+                  child: Container(
+                      height: 48.0,
+                      alignment: Alignment.center,
+                      child: const Text('AppBar Bottom Text')),
+                ),
+              ),
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/big.jpeg'),
+                        fit: BoxFit.fill)),
+              ),
+              title: const Text('AppBar Title'),
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.add_alert),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.phone),
+                  onPressed: () {},
+                ),
+              ]),
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.shifting,
+            //fixed, shifting
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'First',
+                  backgroundColor: Colors.green),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.business),
+                  label: 'Second',
+                  backgroundColor: Colors.red),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: 'Third',
+                  backgroundColor: Colors.purple),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: 'Fourth',
+                  backgroundColor: Colors.pink)
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: _onItemTapped,
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text('Drawer Header'),
+                ),
+                ListTile(
+                  title: const Text('Item 1'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
