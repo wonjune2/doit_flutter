cca8a267d6e078b084fba60e9d4c127973fc12a3
Joon <zzzdnjswns@naver.com>
2023-06-02 21:06:15 +0900

    navigator2

---
+++ flutter_project/lib/ch14/02_navigatot/01.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final bool _isDeepLink = true;
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Navigator(
+        pages: [
+          MaterialPage(child: OneScreen()),
+        ],
+      ),
+    );
+  }
+}
+
+class OneScreen extends StatelessWidget {
+  const OneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('OneScreen'),
+      ),
+      body: Container(
+        color: Colors.red,
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'OneScreen',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TwoScreen extends StatelessWidget {
+  const TwoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TwoScreen'),
+      ),
+      body: Container(
+        color: Colors.cyan,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'TwoScreen',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Pop'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
