851a5f8f21aa26611276ef6fd58a700dedae9e6c
Joon <zzzdnjswns@naver.com>
2023-06-17 01:59:09 +0900

    keyboard input detect test

--- flutter_project/lib/test/test.dart
+++ flutter_project/lib/test/test.dart
@@ -1,39 +1,172 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+/// Flutter code sample for [LogicalKeyboardKey].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const KeyExampleApp());
+
+class KeyExampleApp extends StatelessWidget {
+  const KeyExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final keyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
-    print('keyboard!!: $keyboardOpen');
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Keyboard Status Example'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                keyboardOpen ? 'Keyboard Open' : 'Keyboard Closed',
-                style: const TextStyle(fontSize: 24),
+        appBar: AppBar(title: const Text('Key Handling Example')),
+        body: const MyKeyExample(),
+      ),
+    );
+  }
+}
+
+class MyKeyExample extends StatefulWidget {
+  const MyKeyExample({super.key});
+
+  @override
+  State<MyKeyExample> createState() => _MyKeyExampleState();
+}
+
+class _MyKeyExampleState extends State<MyKeyExample> {
+  // The node used to request the keyboard focus.
+  final FocusNode _focusNode = FocusNode();
+  // The message to display.
+  String? _message;
+
+  // Focus nodes need to be disposed.
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _showDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Focus(
+            focusNode: _focusNode,
+            onKey: _handleKeyEvent,
+            child: Dialog(
+              child: AnimatedBuilder(
+                animation: _focusNode,
+                builder: (BuildContext context, Widget? child) {
+                  if (!_focusNode.hasFocus) {
+                    return GestureDetector(
+                      onTap: () {
+                        FocusScope.of(context).requestFocus(_focusNode);
+                      },
+                      child: const Text('Click to focus'),
+                    );
+                  }
+                  return Text(_message ?? 'Press a key');
+                },
               ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Type something...',
-                ),
-              ),
-            ],
+            ),
+          );
+        });
+  }
+
+  // Handles the key events from the Focus widget and updates the
+  // _message.
+  KeyEventResult _handleKeyEvent(FocusNode node, RawKeyEvent event) {
+    setState(() {
+      print(event.logicalKey.keyId);
+      if (event.logicalKey == LogicalKeyboardKey.keyQ) {
+        _message = 'Pressed the "Q" key!';
+      } else {
+        if (kReleaseMode) {
+          _message =
+              'Not a Q: Pressed 0x${event.logicalKey.keyId.toRadixString(16)}';
+        } else {
+          // As the name implies, the debugName will only print useful
+          // information in debug mode.
+          _message = 'Not a Q: Pressed ${event.logicalKey.debugName}';
+        }
+      }
+    });
+    return event.logicalKey == LogicalKeyboardKey.keyQ
+        ? KeyEventResult.handled
+        : KeyEventResult.ignored;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      color: Colors.white,
+      alignment: Alignment.center,
+      child: DefaultTextStyle(
+        style: textTheme.headlineMedium!,
+        // child: Focus(
+        //   focusNode: _focusNode,
+        //   onKey: _handleKeyEvent,
+        //   child: AnimatedBuilder(
+        //     animation: _focusNode,
+        //     builder: (BuildContext context, Widget? child) {
+        //       if (!_focusNode.hasFocus) {
+        //         return GestureDetector(
+        //           onTap: () {
+        //             FocusScope.of(context).requestFocus(_focusNode);
+        //           },
+        //           child: const Text('Click to focus'),
+        //         );
+        //       }
+        //       return Text(_message ?? 'Press a key');
+        //     },
+        //   ),
+        // ),
+        child: Focus(
+          focusNode: _focusNode,
+          onKey: _handleKeyEvent,
+          child: Dialog(
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Focus(
+                        focusNode: _focusNode,
+                        child: Dialog(
+                          child: SizedBox(
+                            width: 250,
+                            height: 250,
+                            child: GestureDetector(
+                                onTap: () {
+                                  FocusScope.of(context)
+                                      .requestFocus(_focusNode);
+                                },
+                                child: const Text('FOCUS')),
+                          ),
+                        ),
+                      );
+                    });
+                // FocusScope.of(context).requestFocus(_focusNode);
+              },
+              child: const Text('Click to focus'),
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+// Focus(
+//           focusNode: _focusNode,
+//           onKey: _handleKeyEvent,
+//           child: AnimatedBuilder(
+//             animation: _focusNode,
+//             builder: (BuildContext context, Widget? child) {
+//               if (!_focusNode.hasFocus) {
+//                 return GestureDetector(
+//                   onTap: () {
+//                     FocusScope.of(context).requestFocus(_focusNode);
+//                   },
+//                   child: const Text('Click to focus'),
+//                 );
+//               }
+//               return Text(_message ?? 'Press a key');
+//             },
+//           ),
+//         ),
