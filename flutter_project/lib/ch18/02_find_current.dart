e8330990e8d7045f695b2ada2cc095a4c4be23a1
Joon <zzzdnjswns@naver.com>
2023-07-01 00:45:52 +0900

    childWidget

---
+++ flutter_project/lib/ch18/02_find_current.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ParentWidget());
+}
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool favorited = false;
+  int favoriteCount = 10;
+
+  GlobalKey<ChildWidgetState> childKey = GlobalKey();
+  int childCount = 0;
+
+  void toggleFavorite() {
+    setState(() {
+      if (favorited) {
+        favoriteCount -= 1;
+        favorited = false;
+      } else {
+        favoriteCount += 1;
+        favorited = true;
+      }
+    });
+  }
+
+  void getChildData() {
+    ChildWidgetState? childState = childKey.currentState;
+    setState(() {
+      childCount = childState?.childCount ?? 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text('I am Parent, child count: $childCount'),
+            ),
+            ElevatedButton(
+              onPressed: getChildData,
+              child: const Text('Get child data'),
+            ),
+            ChildWidget(key: childKey),
+            const IconWidget(),
+            const ContentWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChildWidget extends StatefulWidget {
+  const ChildWidget({super.key});
+
+  @override
+  State<ChildWidget> createState() => _ChildWidgetState();
+}
+
+class _ChildWidgetState extends State<ChildWidget> {
+  int childCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Text('I am Child, $childCount'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              childCount++;
+            });
+          },
+          child: const Text('increment'),
+        ),
+      ],
+    );
+  }
+}
+
+class IconWidget extends StatelessWidget {
+  const IconWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    _ParentWidgetState? state =
+        context.findAncestorStateOfType<_ParentWidgetState>();
+    return Center(
+      child: IconButton(
+        icon: ((state?.favorited ?? false)
+            ? const Icon(Icons.favorite)
+            : const Icon(
+                Icons.favorite_border,
+              )),
+        onPressed: state?.toggleFavorite,
+      ),
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  const ContentWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    _ParentWidgetState? state =
+        context.findAncestorStateOfType<_ParentWidgetState>();
+    return Center(
+      child: Text('favoriteCount : ${state?.favoriteCount}'),
+    );
+  }
+}
